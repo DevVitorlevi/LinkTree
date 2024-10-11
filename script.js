63800eaf847923930ca3f64822850de496b23acc
Dev Vitor <160429477+DevVitorlevi@users.noreply.github.com>
2024-10-10 21:46:54 -0300

    att

---
+++ script.js
@@ -0,0 +1,13 @@
+const switch_ = document.querySelector('.switch')
+const html=document.documentElement
+let img=document.querySelector('.profile img')
+
+switch_.addEventListener('click',()=>{
+    html.classList.toggle('ligth')
+    
+    if(html.classList.contains('ligth')){
+        img.src='img/avatar-light.png'
+    }else{
+        img.src='./img/avatar.png'
+    }
+})
